8f4d5e13e717cc5e136831900c5a5aa90a6e8288
ayung <ayungyung20@gmail.com>
2021-07-15 15:26:51 +0700

    widget untuk list food vertical

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -11,6 +11,8 @@ class _FoodPageState extends State<FoodPage> {
   int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    double listItemWidth =
+        MediaQuery.of(context).size.width - 2 * defaultMargin;
     return ListView(
       children: [
         Column(
@@ -91,16 +93,20 @@ class _FoodPageState extends State<FoodPage> {
                     height: 16,
                   ),
                   Builder(builder: (_) {
-                    String body = (selectedIndex == 0)
-                        ? 'New Taste Body'
+                    List<Food> foods = (selectedIndex == 0)
+                        ? mockFoods
                         : (selectedIndex == 1)
                             ? 'Popular Body'
                             : 'Recomended Body';
-                    return Center(
-                      child: Text(
-                        body,
-                        style: blackFontStyle2,
-                      ),
+                    return Column(
+                      children: foods
+                          .map((e) => Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    defaultMargin, 0, defaultMargin, 16),
+                                child: FoodListItem(
+                                    food: e, itemWidth: listItemWidth),
+                              ))
+                          .toList(),
                     );
                   }),
                   SizedBox(
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
@@ -10,3 +11,4 @@ part 'bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
 part 'custom_tabbar.dart';
+part 'food_list_item.dart';
