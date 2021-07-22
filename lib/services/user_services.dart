cd66a26e2c14bb38f3e2090d7a212f32b7404fb8
ayung <ayungyung20@gmail.com>
2021-07-22 16:19:53 +0700

    integrasi service

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -4,8 +4,8 @@ class UserServices {
   static Future<ApiReturnValue<User>> signIn(
       String email, String password) async {
     await Future.delayed(Duration(microseconds: 500));
-    // return ApiReturnValue(value: mockUser);
+    return ApiReturnValue(value: mockUser);
 
-    return ApiReturnValue(message: 'Wrong password');
+    // return ApiReturnValue(message: 'Wrong password');
   }
 }
--- lib/ui/pages/order_history_page.dart
+++ lib/ui/pages/order_history_page.dart
@@ -19,74 +19,80 @@ class _OrderHistoryPageState extends State<OrderHistoryPage> {
       .toList();
   @override
   Widget build(BuildContext context) {
-    if (inProgress.length == 0 && past.length == 0) {
-      return IllustrationPage(
-        title: 'Ouch! Hungry',
-        subtitle: 'Seems like you have not\nordered any food yet',
-        picturePath: 'assets/love_burger.png',
-        buttonTitle1: 'Find Foods',
-        buttonTap1: () {},
-      );
-    } else {
-      double listItemWidth =
-          MediaQuery.of(context).size.width - 2 * defaultMargin;
-      return ListView(
-        children: [
-          Column(
+    return BlocBuilder<TransactionCubit, TransactionState>(builder: (_, state) {
+      if (state is TransactionLoaded) {
+        if (inProgress.length == 0 && past.length == 0) {
+          return IllustrationPage(
+            title: 'Ouch! Hungry',
+            subtitle: 'Seems like you have not\nordered any food yet',
+            picturePath: 'assets/love_burger.png',
+            buttonTitle1: 'Find Foods',
+            buttonTap1: () {},
+          );
+        } else {
+          double listItemWidth =
+              MediaQuery.of(context).size.width - 2 * defaultMargin;
+          return ListView(
             children: [
-              //// HEADER
-              Container(
-                height: 100,
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: defaultMargin),
-                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Your Orders', style: blackFontStyle1),
-                    Text('Wait for the best meal', style: greyFontStyle),
-                  ],
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    CustomTabBar(
-                      titles: ['In Progress', 'Past Orders'],
-                      selectedIndex: selectedIndex,
-                      onTap: (index) {
-                        setState(() {
-                          selectedIndex = index;
-                        });
-                      },
+              Column(
+                children: [
+                  //// HEADER
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: defaultMargin),
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Your Orders', style: blackFontStyle1),
+                        Text('Wait for the best meal', style: greyFontStyle),
+                      ],
                     ),
-                    SizedBox(
-                      height: 16,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        CustomTabBar(
+                          titles: ['In Progress', 'Past Orders'],
+                          selectedIndex: selectedIndex,
+                          onTap: (index) {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Column(
+                            children: (selectedIndex == 0 ? inProgress : past)
+                                .map((e) => Padding(
+                                      padding: const EdgeInsets.only(
+                                          right: defaultMargin,
+                                          left: defaultMargin,
+                                          bottom: 16),
+                                      child: OrderListItem(
+                                        transaction: e,
+                                        itemWidth: listItemWidth,
+                                      ),
+                                    ))
+                                .toList())
+                      ],
                     ),
-                    Column(
-                        children: (selectedIndex == 0 ? inProgress : past)
-                            .map((e) => Padding(
-                                  padding: const EdgeInsets.only(
-                                      right: defaultMargin,
-                                      left: defaultMargin,
-                                      bottom: 16),
-                                  child: OrderListItem(
-                                    transaction: e,
-                                    itemWidth: listItemWidth,
-                                  ),
-                                ))
-                            .toList())
-                  ],
-                ),
+                  )
+                ],
               )
             ],
-          )
-        ],
-      );
-    }
+          );
+        }
+      } else {
+        Center(child: loadingIndicator);
+      }
+    });
   }
 }
